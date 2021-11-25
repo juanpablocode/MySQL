44ec4404161757b879fd55d29e3f6e4012ffb34e
Juan Pablo <68447815+juanpablocode@users.noreply.github.com>
2021-11-25 08:14:31 -0300

    V3.0
    Exemplo atualizado.
    Tema: Pokémon

--- dba-juan/exemploAula.sql
+++ dba-juan/exemploAula.sql
@@ -1,45 +1,48 @@
-DROP DATABASE EasyGames;
+DROP DATABASE PokemonGo;
 
-CREATE DATABASE EasyGames;
+CREATE DATABASE PokemonGo;
 
-USE EasyGames; # PARA USAR O DB RECENCRIADO
+USE PokemonGo; # PARA USAR O DB RECENCRIADO
 
-CREATE TABLE items (
-	item_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	item_name VARCHAR(20) NOT NULL,
-	item_atk INT(3) NOT NULL,
-	item_def INT(3) NOT NULL,
-	item_price INT(3) NOT NULL
+CREATE TABLE pokemon (
+	pkm_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	pkm_name VARCHAR(20) NOT NULL,
+	pkm_atk INT(3) NOT NULL,
+	pkm_def INT(3) NOT NULL,
+	pkm_price INT(3) NOT NULL
 );
 
-CREATE TABLE players (
+CREATE TABLE trainers (
 	player_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	player_name VARCHAR(20) NOT NULL,
 	player_life INT(3) NOT NULL,
-	player_mana INT(3) NOT NULL,
-	player_atk INT(3) NOT NULL,
-	player_def INT(3) NOT NULL,
 	player_money INT(3) NOT NULL,
-	item_id INT,
-	FOREIGN KEY (item_id) REFERENCES items (item_id)
+	pkm_id INT,
+	FOREIGN KEY (pkm_id) REFERENCES items (pkm_id)
 );
 
-CREATE TABLE enemies (
+CREATE TABLE challenging (
 	enemy_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	enemy_name VARCHAR(20) NOT NULL,
 	enemy_life INT(3) NOT NULL,
-	enemy_mana INT(3) NOT NULL,
-	enemy_atk INT(3) NOT NULL,
-	enemy_def INT(3) NOT NULL,
 	enemy_money INT(3) NOT NULL,
-	item_id INT,
-	FOREIGN KEY (item_id) REFERENCES items (item_id)
+	pkm_id INT,
+	FOREIGN KEY (pkm_id) REFERENCES items (pkm_id)
 );
 
-DROP TABLES dp_central;  # USADO APENAS PARA APAGAR ARQUIVOS
+CREATE TABLE team_rocket (
+	team_rocket_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	enemy_name VARCHAR(20) NOT NULL,
+	enemy_life INT(3) NOT NULL,
+	enemy_money INT(3) NOT NULL,
+	pkm_id INT,
+	FOREIGN KEY (pkm_id) REFERENCES items (pkm_id)
+);
 
-INSERT INTO tb_datas VALUES(1,'2022-02-20','2022-07-30 12:30:12','2020');
+DROP TABLES trainers;  # USADO APENAS PARA APAGAR TABELAS
 
-SELECT * FROM tb_datas;
+INSERT INTO trainers VALUES(1,'2022-02-20','2022-07-30 12:30:12','2020');
 
-SELECT * FROM tb_funcionarios;
+SELECT * FROM trainers;
+
+SELECT * FROM trainers;
