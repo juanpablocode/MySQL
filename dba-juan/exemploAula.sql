87c4e4557096a94dbeb83d4ce5d525a7d16802ef
Juan Pablo <68447815+juanpablocode@users.noreply.github.com>
2021-11-25 05:49:44 -0300

    V2.0

--- dba-juan/exemploAula.sql
+++ dba-juan/exemploAula.sql
@@ -4,46 +4,42 @@ CREATE DATABASE EasyGames;
 
 USE EasyGames; # PARA USAR O DB RECENCRIADO
 
-CREATE TABLE if not exists dp_central (
-	id_data 		INT UNSIGNED ZEROFILL,
-    data_inicio		DATE,
-    data_termino	DATETIME,
-    ano 			YEAR
-); 
+CREATE TABLE items (
+	item_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	item_name VARCHAR(20) NOT NULL,
+	item_atk INT(3) NOT NULL,
+	item_def INT(3) NOT NULL,
+	item_price INT(3) NOT NULL
+);
 
-# USADO NA AULA
-CREATE TABLE tb_departamento (
-	id_dep_cod		INT SIGNED not null auto_increment,
-    dep_nome	VARCHAR(50) NOT NULL,
-    
-primary key (id_dep_cod));
+CREATE TABLE players (
+	player_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	player_name VARCHAR(20) NOT NULL,
+	player_life INT(3) NOT NULL,
+	player_mana INT(3) NOT NULL,
+	player_atk INT(3) NOT NULL,
+	player_def INT(3) NOT NULL,
+	player_money INT(3) NOT NULL,
+	item_id INT,
+	FOREIGN KEY (item_id) REFERENCES items (item_id)
+);
 
-# USADO NA AULA
-CREATE TABLE tb_funcionario (
-	id_fun		INT not null auto_increment,
-    dep_nome	VARCHAR(50) NOT NULL,
-    
-primary key (id_dep_cod));
+CREATE TABLE enemies (
+	enemy_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	enemy_name VARCHAR(20) NOT NULL,
+	enemy_life INT(3) NOT NULL,
+	enemy_mana INT(3) NOT NULL,
+	enemy_atk INT(3) NOT NULL,
+	enemy_def INT(3) NOT NULL,
+	enemy_money INT(3) NOT NULL,
+	item_id INT,
+	FOREIGN KEY (item_id) REFERENCES items (item_id)
+);
 
 DROP TABLES dp_central;  # USADO APENAS PARA APAGAR ARQUIVOS
 
-
-
-
 INSERT INTO tb_datas VALUES(1,'2022-02-20','2022-07-30 12:30:12','2020');
 
-CREATE TABLE tb_funcionarios(
-	id_func		INT SIGNED,
-    nome_func	VARCHAR(200),
-    email_func	VARCHAR(250),
-    cep_func	char(9),
-    sexo_func	enum('M','F')
-);
-
 SELECT * FROM tb_datas;
-
-INSERT INTO tb_funcionarios VALUES(
-	-10,'Juan Pablo','juanoliveira110@gmail.com','01311-100','M'
-);
 
 SELECT * FROM tb_funcionarios;
